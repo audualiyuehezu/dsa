c18574849c00a18e42e192ba98ea4344c4c91606
audualiyuehezu <alidevopsolid@outlook.com>
2022-12-17 07:16:30 -0500

    push first

---
+++ search_arr.py
@@ -0,0 +1,16 @@
+def search(arr,n,x):
+    for i in range(0,n):
+         if arr[i] == x:
+            return i
+    return -1
+
+if  __name__ == "__main__":
+    arr = [2,4,6,1,7]
+    n = len(arr)
+    x = 4
+
+    ans = search(arr, n, x)
+    if ans == -1:
+       print("The array index has not been found")
+    else:
+        print(f"The array index is found")
