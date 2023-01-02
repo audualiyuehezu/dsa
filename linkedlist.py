7e733dfd89de4751dad71c4e4c37833be02fce74
audualiyuehezu <alidevopsolid@outlook.com>
2023-01-01 23:36:00 -0500

    update

--- linkedlist.py
+++ linkedlist.py
@@ -2,7 +2,9 @@ from collections import deque
 
 linkedlistr = deque()
 
-linkedlistr("flask")
-linkedlistr("fastapi")
+linkedlistr.append(7)
+linkedlistr.append(9)
+linkedlistr.append(8)
+linkedlistr.appendleft(6)
 
-print(linkedlistr)
+print(linkedlistr.maxlen)
